411ef70ed7d317f3d4dc5242998215a8f0388e87
Akshat Soni <akshatsoni1304@gmail.com>
2026-08-18 13:34:20 +0530

    Time: 2 ms (72.63%), Space: 45.2 MB (53.33%) - LeetHub

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
@@ -2,9 +2,11 @@ class Solution {
     public int largestInteger(int[] nums, int k) {
         int n = nums.length;
         HashMap<Integer, Integer> map = new HashMap<>();
+        
         for(int i=0; i<n; i++){                         //put the array into map
             map.put(nums[i], map.getOrDefault(nums[i],0)+1);
         }
+
         if(k==n){                                       //if given subarray size equals the size of array
             int maxVal = Integer.MIN_VALUE;             //set maxVal as minimum intially
             for(int i=0; i<n; i++){
@@ -12,6 +14,7 @@ class Solution {
             }
             return maxVal;
         }
+
         if(k==1){              //if subarray size is 1 then check the element with count as 1 and then return the one having maxVal and count 1
             int maxVal=-1;
             for(int i=0; i<n; i++){
@@ -21,19 +24,21 @@ class Solution {
             }
             return maxVal;
         }
-        n=n-1;
-        if(nums[0]==nums[n]){
-            return -1;
+        n=n-1;                               //setting n to n-1 to get the index of last element since as of now n is pointing to n is size
+                                            //of array then if we didn't do n-1 then nums[n] would go out of bound for checking the element..
+
+        if(nums[0]==nums[n]){                   //*If both ends hold the same value, that value's count is automatically ≥ 2, 
+            return -1;                          //so neither end can be unique → no candidate exists → -1.
         }
         if(map.get(nums[0])==1 && map.get(nums[n])==1){
-            return Math.max(nums[0],nums[n]);
+            return Math.max(nums[0],nums[n]);                  // both ends unique → take the larger
         }
         if(map.get(nums[0])==1 && map.get(nums[n])>1){
-            return nums[0];
+            return nums[0];                                    // only left end unique
         }
         if(map.get(nums[0])>1 && map.get(nums[n])==1){
-            return nums[n];
+            return nums[n];                                    // only right end unique
         }
-        return -1;
+        return -1;                                             // neither end unique → no candidate
     }
 }
