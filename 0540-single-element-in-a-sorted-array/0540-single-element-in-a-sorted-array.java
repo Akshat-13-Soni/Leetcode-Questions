93106db8ed02e62b8f37271a4dedb34dc87fe067
Akshat Soni <akshatsoni1304@gmail.com>
2026-09-06 16:47:45 +0530

    Time: 0 ms (100%), Space: 52.7 MB (77.1%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -4,23 +4,36 @@ class Solution {
         if(n==1){
             return nums[0];
         }
-        for(int i=0; i<n; i++){
-            if(i==0){
-                if(nums[i]!=nums[i+1]){
-                    return nums[i];
-                }
+        //leaving the first & last indexes to optimise code and write a cleaner code
+        if(nums[0]!=nums[1]){             
+            return nums[0];
+        }
+        if(nums[n-1]!=nums[n-2]){
+            return nums[n-1];
+        }
+        int low=1, high=n-2;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]){
+                return nums[mid];
             }
-            else if(i==n-1){
-                if(nums[i]!=nums[i-1]){
-                    return nums[i];
+            if(mid%2==0){
+                if(nums[mid]==nums[mid-1]){
+                    high=mid-1;
+                }
+                else{
+                    low=mid+1;
                 }
             }
             else{
-                if(nums[i]!=nums[i+1] && nums[i]!=nums[i-1]){
-                    return nums[i];
+                if(nums[mid]==nums[mid-1]){
+                    low=mid+1;
+                }
+                else{
+                    high=mid-1;
                 }
             }
         }
-        return n;
+        return nums[low];
     }
 }
