5da1ac57e0337688a272362e2430e10fa8c94f18
Akshat Soni <akshatsoni1304@gmail.com>
2026-09-08 22:58:55 +0530

    Time: 0 ms (100%), Space: 42.5 MB (72.85%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,13 +1,10 @@
 class Solution {
     public int countCommas(int n) {
-        int total = 0;
-
-        for (int i = 1; i <= n; i++) {
-            int digitCount = String.valueOf(i).length();  // how many digits does i have?
-            int commas = (digitCount - 1) / 3;             // how many commas does i get?
-            total += commas;                                // add it to the running total
+        if(n<1000){
+            return 0;
         }
-
-        return total;
+        else{
+            return n-999;
+        }
     }
 }
