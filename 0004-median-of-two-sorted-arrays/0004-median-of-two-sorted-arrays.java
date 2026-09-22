46d66bfd0071a0499977f0ee9a309b3796204d68
Akshat Soni <akshatsoni1304@gmail.com>
2026-09-22 23:47:09 +0530

    Time: 1 ms (100%), Space: 48.8 MB (71.05%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -2,31 +2,44 @@ class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int m = nums1.length;
         int n = nums2.length;
-        int[] mergedArray = new int[m+n];
-        int i = 0, j = 0, k = 0;
-        // Compare and merge elements
-        while (i < nums1.length && j < nums2.length) {
-            if (nums1[i] <= nums2[j]) {
-                mergedArray[k++] = nums1[i++];
-            } 
-            else {
-                mergedArray[k++] = nums2[j++];
+        if(m>n){
+            return findMedianSortedArrays(nums2, nums1);
+        }
+        int low=0, high=m;
+        int left = (m+n+1)/2;
+        int total = m+n;
+        while(low<=high){
+            int mid1 = low+(high-low)/2;
+            int mid2 = left-mid1;
+            int l1 = Integer.MIN_VALUE, l2 = Integer.MIN_VALUE;
+            int r1 = Integer.MAX_VALUE, r2 = Integer.MAX_VALUE;
+            if(mid1<m){
+                r1 = nums1[mid1];
+            }
+            if(mid2<n){
+                r2 = nums2[mid2];
+            }
+            if(mid1-1>=0){
+                l1 = nums1[mid1-1];
+            }
+            if(mid2-1>=0){
+                l2 = nums2[mid2-1];
+            }
+            if(l1<=r2 && l2<=r1){
+                if(total%2==0){
+                    return ((double)Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
+                }
+                else{
+                    return (double)Math.max(l1,l2);
+                }
+            }
+            else if(l1>r2){
+                high = mid1-1;
+            }
+            else{
+                low=mid1+1;
             }
         }
-        // Copy remaining elements from arr1
-        while (i < nums1.length) {
-            mergedArray[k++] = nums1[i++];
-        }
-        // Copy remaining elements from arr2
-        while (j < nums2.length) {
-            mergedArray[k++] = nums2[j++];
-        }
-        int totalLen = m+n;
-        if(totalLen%2==0){
-            return ((double)mergedArray[totalLen/2-1] + mergedArray[totalLen/2])/2.0;
-        } 
-        else{
-            return mergedArray[totalLen/2];
-        }
+        return 0.0;
     }
 }
